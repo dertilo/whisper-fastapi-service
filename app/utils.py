58cfa7f1c30bf1e260f0f2aa71394c4d45f845d5
Tilo Himmelsbach <dertilo@gmail.com>
2022-10-15 15:09:12 +0200

    load_audio

---
+++ app/utils.py
@@ -0,0 +1,21 @@
+import ffmpeg
+import numpy as np
+from whisper.audio import SAMPLE_RATE
+
+
+def load_audio(file: str, sr: int = SAMPLE_RATE):
+    """
+    based on: https://github.com/openai/whisper/blob/d18e9ea5dd2ca57c697e8e55f9e654f06ede25d0/whisper/audio.py#L22
+    """
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+        )
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
